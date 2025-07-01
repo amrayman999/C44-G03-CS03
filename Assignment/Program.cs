1ad8524d43ff339916de8307cab4cff892c29fe8
amrayman1999 <amr.ayman.essawi.1999@gmail.com>
2025-07-01 17:55:07 +0300

    Question (3)

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -8,15 +8,23 @@
             //Console.WriteLine("Enter Number: ");
             //Console.WriteLine(Console.ReadLine());
             #endregion
+
             #region Question (2)
             // if we used convert or parse it will throw exception
             // but using tryparse , if it is parsed it return true out parameter contain parsed number
             // , if not it returns false and output paramter contains 0 (the default value of int)
-            string numString = Console.ReadLine();
-            bool isParsed = int.TryParse(numString, out int result);
-            Console.WriteLine(isParsed);
+            //string numString = Console.ReadLine();
+            //bool isParsed = int.TryParse(numString, out int result);
+            //Console.WriteLine(isParsed);
+            //Console.WriteLine(result);
+            #endregion
+
+            #region Question (3)
+            // in this case we use float (f) to represent any floating point number as float, as the result is float)
+            float result = 3.5f + 5 - 2 + 3.555f;
             Console.WriteLine(result);
             #endregion
+
 
 
         }
