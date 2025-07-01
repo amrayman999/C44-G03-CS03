f792b2563d278f7d51624e1ad9dd1233ecb7ef89
amrayman1999 <amr.ayman.essawi.1999@gmail.com>
2025-07-01 17:46:50 +0300

    Question (1)

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -4,7 +4,10 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Question (1)
+            Console.WriteLine("Enter Number: ");
+            Console.WriteLine(Console.ReadLine());
+            #endregion
         }
     }
 }
