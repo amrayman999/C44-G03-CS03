68d5e9ebd0aebc487342ac430b4389f681172fba
amrayman1999 <amr.ayman.essawi.1999@gmail.com>
2025-07-01 15:10:51 +0300

    Part 01 Fractions & Discards

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,10 +1,21 @@
-﻿namespace Demo
+﻿using System.Globalization;
+
+namespace Demo
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Part 01 Fractions & Discards
+            //float X = 3.5587847458f;
+            //double Y = 3.5;
+            //decimal Z = 3.5m;
+            //Console.WriteLine(X);
+            long X = 781_423_521_564;
+            Console.WriteLine($"{X:c}");
+            var cultureInfo = new CultureInfo("ar-SA");
+            Console.WriteLine(X.ToString("c", cultureInfo));
+            #endregion
         }
     }
 }
