de216e03d8cac38ce3cde9f6861b2c4040c9d4d3
amrayman1999 <amr.ayman.essawi.1999@gmail.com>
2025-07-01 15:20:12 +0300

    Part 02 Implicit & Explicit - Casting

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -11,10 +11,34 @@ namespace Demo
             //double Y = 3.5;
             //decimal Z = 3.5m;
             //Console.WriteLine(X);
-            long X = 781_423_521_564;
-            Console.WriteLine($"{X:c}");
-            var cultureInfo = new CultureInfo("ar-SA");
-            Console.WriteLine(X.ToString("c", cultureInfo));
+            //long X = 781_423_521_564;
+            //Console.WriteLine($"{X:c}");
+            //var cultureInfo = new CultureInfo("ar-SA");
+            //Console.WriteLine(X.ToString("c", cultureInfo));
+            #endregion
+
+            #region Part 02 Implicit & Explicit - Casting
+            //int x = 1000; // 4 bytes
+            //long y = x; // 8 bytes - implicit casting
+            //console.writeline(y);
+            //long z = 514587845554544;
+            //int w = (int)z; // explicit casting - unsafe casting
+            //console.writeline(w);
+            // overflow - data loss
+            //unchecked
+            //{
+            //    long X = 25154564548484;
+            //    int Y = (int)X;
+            //    Console.WriteLine(Y);
+            //}
+            long X = 5454785784545;
+            if (X > int.MaxValue || X < int.MinValue)
+                Console.WriteLine("exception will be thrown");
+            else
+            {
+                int Y = (int)X;
+                Console.WriteLine(Y);  
+            }
             #endregion
         }
     }
